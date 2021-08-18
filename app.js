ec43ce948dd7fff68eeeec30f31202879895cb9e
Farhan <farhanahad3@gmail.com>
2021-08-18 15:50:15 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+//console.log('I am in a separate file')
+
+function updateProductNumber(product,price,isIncreasing){
+
+    const productInput=document.getElementById(product+'-number');
+    let productNumber=productInput.value;
+
+    if(isIncreasing){
+
+        productNumber=parseInt(productNumber)+1;
+    }
+    else if(productNumber>0){
+
+        productNumber=parseInt(productNumber)-1;
+    }
+
+    productInput.value=productNumber;
+    //update  total
+    const productTotal=document.getElementById(product+'-total');
+    productTotal.innerText =productNumber * price;
+
+    //calculate total
+    calculateTotal();
+}
+function getInputValue(product){
+    const productInput=document.getElementById(product+'-number');
+    const productNumber=parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal(){
+   
+    const phoneTotal=getInputValue('phone')*1219;
+    const caseTotal=getInputValue('case')*59;
+    const subTotal=phoneTotal+caseTotal;
+    const tax=subTotal/10;
+    const totalPrice=subTotal+tax;
+    //console.log(subTotal);
+    //update on the html
+
+    document.getElementById('sub-total').innerText=subTotal;
+    document.getElementById('tax-amount').innerText=tax;
+    document.getElementById('total-price').innerText=totalPrice;
+
+}
+
+//phone increase decrease event
+document.getElementById('phone-plus').addEventListener('click',function(){
+    console.log('phone plus clicked');
+    updateProductNumber('phone',1219,true)
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    console.log('phone plus clicked');
+    updateProductNumber('phone',1219,false);
+})
+
+//handle case increase decrease event
+
+document.getElementById("case-plus").addEventListener('click',function(){
+  // console.log('clicked');
+  updateProductNumber('case',59,true);
+});
+
+document.getElementById('case-minus').addEventListener('click',function(){
+    updateProductNumber('case',59,false);
+    // 
+    // console.log(caseNumber);
+})
